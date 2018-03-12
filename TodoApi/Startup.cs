6b8fe4d5cb57fc4bc8e1ac54c1c2041992208093
Tommy Langhelle <tomlanghe@gmail.com>
2018-03-12 21:27:24 +0100

    Initial commit

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(
+                "Server = tcp:dbapp-001.database.windows.net; Database=TestApp;  User ID = tommy; Password =towela100889?!; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"
+                ));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
